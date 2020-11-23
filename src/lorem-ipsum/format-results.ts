381c0bb9d4dce6ec45ace9d6ce205f29eed54cc2
Misha Bruml <misha.bruml@ecotricity.co.uk>
2020-11-23 15:58:44 +0000

    chalk for colour

--- src/lorem-ipsum/format-results.ts
+++ src/lorem-ipsum/format-results.ts
@@ -1,4 +1,5 @@
 import { TextStatistics } from './model'
+import chalk from 'chalk'
 
 export const formatStatistics = ({
   nSentences,
@@ -9,11 +10,11 @@ export const formatStatistics = ({
   averageWordsPerSentence,
   mostThreeFrequentPhrases,
   mostFiveFrequentWordsProminence
-}: TextStatistics) => `The number of sentences is: ${nSentences}
-The number of words is: ${nWords}
-The longest word is: "${longestWord}" and its length is: ${longestWord.length}
-The 6 most frequently occuring words in the text are: [ ${mostSixFrequentWords.map(s => `"${s}"`)} ]
-The percentage of the words only occuring once is: ${percentageUniqueWords}%
-The average number of words per sentence is: ${averageWordsPerSentence}
-The three most frequently occuring two-word phrases in the text are: [ ${mostThreeFrequentPhrases.map(s => `"${s}"`)} ]
-The five most frequent words in the text and their prominence are: ${mostFiveFrequentWordsProminence.map(o => `[ word: "${o.word}", prominence: ${o.prominence}% ]`)}`
+}: TextStatistics) => chalk`The number of sentences is: {yellow ${nSentences}}
+The number of words is: {yellow ${nWords}}
+The longest word is: {green "${longestWord}"} and its length is: {yellow ${longestWord.length}}
+The 6 most frequently occuring words in the text are: [${mostSixFrequentWords.map(s => chalk.green` "${s}"`)} ]
+The percentage of the words only occuring once is: {yellow ${percentageUniqueWords}%}
+The average number of words per sentence is: {yellow ${averageWordsPerSentence}}
+The three most frequently occuring two-word phrases in the text are: [${mostThreeFrequentPhrases.map(s => chalk.green` "${s}"`)} ]
+The five most frequent words in the text and their prominence are: [${mostFiveFrequentWordsProminence.map(o => chalk` word: {green "${o.word}"}, prominence: {yellow ${o.prominence}%}`)}]`
